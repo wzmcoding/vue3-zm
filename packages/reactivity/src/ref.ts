a21cba971574fa28e748bc772dafac8808a78410
wzm <2791063637@qq.com>
2025-06-18 21:33:26 +0800

    fix: 把链表应用到响应式系统中

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,4 +1,5 @@
 import { activeSub } from './effect'
+import { Link, link, propagate } from './system'
 
 enum ReactiveFlags {
   // 属性标记，用于表示对象是不是一个ref
@@ -14,8 +15,16 @@ class RefImpl {
   // ref标记，证明是一个ref
   [ReactiveFlags.IS_REF] = true
 
-  // 保存和 effect 之间的关联关系
-  subs
+  /**
+   * 订阅者链表的头节点，理解为 head
+   */
+  subs: Link
+
+  /**
+   * 订阅者链表的尾节点，理解为 tail
+   */
+  subsTail: Link
+
   constructor(value) {
     this._value = value
   }
@@ -23,8 +32,7 @@ class RefImpl {
   get value() {
     // 收集依赖
     if (activeSub) {
-      // 如果 activeSub 有，那就保存起来， 等我更新的时候，触发
-      this.subs = activeSub
+      trackRef(this)
     }
     return this._value
   }
@@ -32,8 +40,7 @@ class RefImpl {
   set value(newValue) {
     // 触发更新
     this._value = newValue
-    // 通知 effect 重新执行,获取到最新的值
-    this.subs?.()
+    triggerRef(this)
   }
 }
 
@@ -47,4 +54,24 @@ export function ref(value) {
  */
 export function isRef(value) {
   return !!(value && value[ReactiveFlags.IS_REF])
+}
+
+/**
+ * 收集依赖，建立 ref 和 effect 之间的链表联系
+ * @param dep
+ */
+export function trackRef(dep) {
+  if (activeSub) {
+    link(dep, activeSub)
+  }
+}
+
+/**
+ * 触发 ref 关联的 effect 重新执行
+ * @param dep
+ */
+export function triggerRef(dep) {
+  if (dep.subs) {
+    propagate(dep.subs)
+  }
 }
---
+++ packages/reactivity/src/system.ts
@@ -0,0 +1,51 @@
+export interface Link {
+  // 保存当前要关联的 effect
+  sub: Function
+  // 链表的下一个节点
+  nextSub: Link
+  // 链表的上一个节点
+  prevSub: Link
+}
+
+/**
+ * 链接链表关系
+ * @param dep
+ * @param sub
+ */
+export function link(dep, sub) {
+  // 如果 activeSub 有，那就保存起来，等我更新的时候，触发
+  const newLink = {
+    sub,
+    nextSub: undefined,
+    prevSub: undefined,
+  }
+
+  /**
+   * 关联链表关系，分两种情况
+   * 1. 尾节点有，那就往尾节点后面加
+   * 2. 如果尾节点没有，则表示第一次关联，那就往头节点加，头尾相同
+   */
+  if (dep.subsTail) {
+    dep.subsTail.nextSub = newLink
+    newLink.prevSub = dep.subsTail
+    dep.subsTail = newLink
+  } else {
+    dep.subs = newLink
+    dep.subsTail = newLink
+  }
+}
+
+/**
+ * 传播更新的函数
+ * @param subs
+ */
+export function propagate(subs) {
+  let link = subs
+  let queuedEffect = []
+  while (link) {
+    queuedEffect.push(link.sub)
+    link = link.nextSub
+  }
+
+  queuedEffect.forEach(effect => effect())
+}
