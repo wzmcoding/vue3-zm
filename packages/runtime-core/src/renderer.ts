2d0920554d44f726f982dfdf6dd42824b92f005e
wzm <2791063637@qq.com>
2025-08-19 01:41:10 +0800

    feat(runtime-core): 实现双端 diff
    - 添加 patchKeyedChildren 函数实现全量 diff 逻辑
    - 优化了数组子节点的更新逻辑- 新增双端对比算法，提高 diff 效率

--- packages/runtime-core/src/renderer.ts
+++ packages/runtime-core/src/renderer.ts
@@ -72,7 +72,7 @@ export function createRenderer(options) {
           if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
             // 新的也是数组
             // TODO 全量 diff
-            console.log('全量 diff 算法', n1, n2)
+            patchKeyedChildren(n1.children, n2.children, el)
           } else {
             // 老的是数组，新的是 null, 卸载老的数组
             unmountChildren(n1.children)
@@ -86,6 +86,89 @@ export function createRenderer(options) {
         }
       }
     }
+  }
+
+  // 全量diff
+  const patchKeyedChildren = (c1, c2, container) => {
+    console.log('全量 diff 逻辑 c1,c2->', c1, c2)
+    /**
+     * 全量diff
+     *  1. 双端 diff
+     *      1.1 头部对比
+     *      c1 => [a, b]
+     *      c2 => [a, b, c]
+     *      开始时： i= 0, e1= 1, e2= 2
+     *      1.2 尾部对比
+     *      c1 => [a, b]
+     *      c2 => [c, a, b]
+     *      开始时： i= 0, e1= 1, e2= 2
+     */
+
+    // 开始对比的下标
+    let i = 0
+    // 老的子节点的最后一个元素的下标
+    let e1 = c1.length - 1
+    // 新的子节点的最后一个元素的下标
+    let e2 = c2.length - 1
+
+    /**
+     * 1.1 头部对比
+     *      c1 => [a, b]
+     *      c2 => [a, b, c]
+     *      开始时： i= 0, e1= 1, e2= 2
+     *      结束时： i= 2, e1= 1, e2= 2
+     */
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[i]
+      const n2 = c2[i]
+      if (isSameVNodeType(n1, n2)) {
+        // 如果 n1 和 n2 是同一个类型的子节点，那就可以更新, 更新完了之后，对比下一个
+        patch(n1, n2, container)
+      } else {
+        break
+      }
+      i++
+    }
+
+    /**
+     *  1.2 尾部对比
+     *    c1 => [a, b]
+     *    c2 => [c, a, b]
+     *    开始时： i= 0, e1= 1, e2= 2
+     *    结束时： i= 0, e1= -1, e2= 0
+     */
+    while (i <= e1 && i <= e2) {
+      const n1 = c1[e1]
+      const n2 = c2[e2]
+      if (isSameVNodeType(n1, n2)) {
+        // 如果 n1 和 n2 是同一个类型的子节点，那就可以更新， 更新完了之后，对比上一个
+        patch(n1, n2, container)
+      } else {
+        break
+      }
+      // 更新 尾指针
+      e1--
+      e2--
+    }
+
+    if (i > e1) {
+      // 说明老的少，新的多，要挂载新的， 挂载的范围是 i到 e2
+
+      const nextPos = e2 + 1
+      const anchor = nextPos < c2.length ? c2[nextPos].el : null
+      console.log('anchor->', anchor)
+      while (i <= e2) {
+        patch(null, c2[i], container, anchor)
+        i++
+      }
+    } else if (i > e2) {
+      // 说明新的少，老的多，要卸载老的， 卸载的范围是 i 到 e1
+      while (i <= e1) {
+        unmount(c1[i])
+        i++
+      }
+    }
+    console.log('i,e1,e2->', i, e1, e2)
   }
 
   // 更新
@@ -134,7 +217,7 @@ export function createRenderer(options) {
   }
 
   // 挂载
-  const mountElement = (vnode, container) => {
+  const mountElement = (vnode, container, anchor) => {
     /**
      * 1. 创建一个 dom 节点
      * 2. 设置它的 props
@@ -161,7 +244,7 @@ export function createRenderer(options) {
     }
 
     // 插入元素, 挂载到容器中
-    hostInsert(el, container)
+    hostInsert(el, container, anchor)
   }
 
   /**
@@ -169,8 +252,9 @@ export function createRenderer(options) {
    * @param n1 老节点，之前的，如果有，表示要和 n2 做 diff,更新，如果没有，表示直接挂载n2
    * @param n2 新节点
    * @param container 要挂载的容器
+   * @param anchor
    */
-  const patch = (n1, n2, container) => {
+  const patch = (n1, n2, container, anchor = null) => {
     if (n1 === n2) {
       // 如果两次传递了同一个虚拟节点，啥也不干
       return
@@ -183,7 +267,7 @@ export function createRenderer(options) {
 
     if (n1 == null) {
       // 挂载
-      mountElement(n2, container)
+      mountElement(n2, container, anchor)
     } else {
       // 更新
       patchElement(n1, n2)
