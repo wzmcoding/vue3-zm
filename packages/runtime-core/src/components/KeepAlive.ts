db82e5412bab07a1735908e896d97e23e4939307
wzm <2791063637@qq.com>
2025-09-15 10:06:24 +0800

    feat(runtime-core): 添加 KeepAlive组件功能
    - 在 runtime-core 中实现 KeepAlive 组件
    - 添加组件缓存逻辑，支持按组件类型或 key 缓存
    - 在 20-demo.html 中更新 KeepAlive 组件的导入路径

--- packages/runtime-core/src/components/KeepAlive.ts
+++ packages/runtime-core/src/components/KeepAlive.ts
@@ -1,11 +1,27 @@
-export const isKeepAlive = (type) => type?.__isKeepAlive
+export const isKeepAlive = type => type?.__isKeepAlive
 
 export const KeepAlive = {
   name: 'KeepAlive',
   __isKeepAlive: true,
   setup(props, { slots }) {
+    /**
+     * 缓存:
+     * component => vnode
+     * or
+     * key => vnode
+     */
+    const cache = new Map()
+
     return () => {
-      return () => slots.default()
+      const vnode = slots.default()
+
+      const key = vnode.key != null ? vnode.key : vnode.type
+      /**
+       * 在这里处理缓存
+       */
+      cache.set(key, vnode)
+      console.log('cache', cache)
+      return vnode
     }
-  }
+  },
 }
