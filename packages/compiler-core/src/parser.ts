a7cb6c78a9c7ff1f15f7cf4b17f60e94887c8ff6
happyWang <zhengminw345@gmail.com>
2025-11-21 10:59:24 +0800

    feat(compiler-core): 实现编译文本

--- packages/compiler-core/src/parser.ts
+++ packages/compiler-core/src/parser.ts
@@ -1,4 +1,20 @@
 import { NodeTypes } from './ast'
+import { Tokenizer } from './tokenizer'
+
+let currentInput = ''
+let currentRoot
+
+function getSlice(start, end) {
+  return currentInput.slice(start, end)
+}
+
+function getLoc(start, end) {
+  return {
+    start: tokenizer.getPos(start), // 开始的位置信息
+    end: tokenizer.getPos(end), // 结束的位置信息
+    source: getSlice(start, end), // 内容
+  }
+}
 
 /**
  * 创建ast语法树的根节点
@@ -15,6 +31,30 @@ function createRoot(source) {
   }
 }
 
+const tokenizer = new Tokenizer({
+  ontext(start, end) {
+    const content = getSlice(start, end)
+
+    const textNode = {
+      content,
+      type: NodeTypes.TEXT,
+      loc: getLoc(start, end),
+    }
+    currentRoot.children.push(textNode)
+  },
+})
+
 export function parse(input) {
-  console.log('parse', input)
+  // 把当前正在解析的字符串暴漏给外部作用域
+  currentInput = input
+  const root = createRoot(input)
+  // 把当前创建的根节点暴漏给外部作用域
+  currentRoot = root
+  /**
+   * 开始解析 input
+   * 帮我们解析
+   */
+  tokenizer.parse(input)
+
+  return root
 }
